0b05d7f0e3ba35cd569d41023ac7d2b2fc33cbc8
Rob Rouland <robrouland@icloud.com>
2014-07-26 22:52:55 -0700

    Install devise, add a user, and install devise views.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  #added above action_mailer for devise auth package
 end
